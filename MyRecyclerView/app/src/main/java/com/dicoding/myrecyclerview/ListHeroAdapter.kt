78bce0f3de31fd1d75aaf056432fdc370704c4dd
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-10-03 14:47:32 +0700

    latihan glide

--- MyRecyclerView/app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
+++ MyRecyclerView/app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
@@ -23,7 +24,11 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+
+        Glide.with(holder.itemView.context)
+            .load(photo) // URL Gambar
+            .into(holder.imgPhoto) // imgview mana yang akan diterapkan
+
         holder.tvName.text = name
         holder.tvDescription.text = description
 
